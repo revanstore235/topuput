5908fb60756ec4a8761aee7e9e82ac6671e65933
revanstore235 <revindradra@gmail.com>
2025-10-25 15:35:07 +0700

    Add files via upload

---
+++ topup.py
@@ -0,0 +1,20 @@
+Gini cuy contoh dari aing
+cara enc 
+cd /sdcard
+rm -rf Py-Fuscate
+git clone https://github.com/Sl-Sanda-Ru/Py-Fuscate
+cd Py-Fuscate
+ls
+nano 1.py
+python py_fuscate.py -i 2.py -o revanstore.py -c 200
+Kalau mau enc yang lain
+ganti bagian baris 254 dengan github nama repo github lu
+cara enc 
+cd /sdcard
+rm -rf Py-Fuscate
+git clone https://github.com/Sl-Sanda-Ru/Py-Fuscate
+cd Py-Fuscate
+ls
+nano 2.py
+python py_fuscate.py -i 2.py -o bussidrose.py -c 300 
+Nah yang ini kan posisi 300 lu bisa ubah lebih kecil biar jadi kb contoh nya 150/ 200 san
